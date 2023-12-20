22eecd0d04897132f406194be45edd80f1ae869e
Rubel Mahmud <vxlrubel@gmail.com>
2023-12-21 00:20:36 +0600

    create custom.js file for custom script

---
+++ assets/js/custom.js
@@ -0,0 +1,14 @@
+;(function($){
+    // define constant
+    const doc = $(document);
+
+    class Ecommerce_Html_Template{
+        init(){}
+    }
+
+    // initiate the programs after document ready
+    doc.ready(()=>{
+        const ecommerce_template = new Ecommerce_Html_Template();
+        ecommerce_template.init();
+    });
+})(jQuery);
